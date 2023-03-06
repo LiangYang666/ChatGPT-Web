ffbc493efe3b5b9da5b201c18986775cc2188fb1
yl_pc_deepin <mail.liangyang@gmail.com>
2023-03-06 11:00:38 +0800

    实现用户级别存储，互不干扰，多会话也可重载某一用户聊天记录

---
+++ LRU_cache.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.stack = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key):
+        if key in self.stack:
+            self.stack.move_to_end(key)
+            return self.stack[key]
+        else:
+            return None
+
+    def put(self, key, value) -> None:
+        if key in self.stack:
+            self.stack[key] = value
+            self.stack.move_to_end(key)
+        else:
+            self.stack[key] = value
+        if len(self.stack) > self.capacity:
+            self.stack.popitem(last=False)
+
+    def __contains__(self, key):
+        return key in self.stack
+
+
+if __name__ == '__main__':
+    cache = LRUCache(2)
+
+    cache.put(0, 1)
+    cache.put(1, 1)
+    cache.put(2, 1)
+    print(0 in cache)
--- flask_main.py
+++ flask_main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import os
 import openai
 import sys
 import uuid
+from LRU_cache import LRUCache
+import threading
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -11,7 +13,9 @@ os.environ['HTTP_PROXY'] = 'socks5://127.0.0.1:7890'
 os.environ['HTTPS_PROXY'] = 'socks5://127.0.0.1:7890'
 openai.api_key = os.getenv("OPENAI_API_KEY")        # 从环境变量中获取api_key,或直接设置api_key
 
-chat_context_number = 5     # 连续对话模式下的上下文数量
+chat_context_number = 5         # 连续对话模式下的上下文数量
+all_user_dict = LRUCache(12)    # 设置最多存储12个用户的上下文
+lock = threading.Lock()         # 用于线程锁
 
 
 def get_response_from_ChatGPT_API(message_context):
@@ -28,7 +32,13 @@ def get_response_from_ChatGPT_API(message_context):
 
 
 def handle_messages_get_response(message, message_history, chat_with_history):
-
+    """
+    处理用户发送的消息，获取回复
+    :param message:
+    :param message_history:
+    :param chat_with_history:
+    :return:
+    """
     message_history.append({"role": "user", "content": message})
     message_context = []
     if chat_with_history:
@@ -58,16 +68,35 @@ def check_session(current_session):
     :param current_session: 当前session
     :return: 当前session
     """
-    if current_session.get('userid') is not None:   # 如果session存在
-        userid = current_session.get('userid')
-        print("existing session, userid:\t", userid)
-    else:   # 如果session不存在
-        userid = uuid.uuid1()
-        current_session['userid'] = userid
-        current_session['chat_with_history'] = False
-        current_session['messages_history'] = []
-        print("new session, userid:\t", userid)
-    return current_session
+    if current_session.get('session_id') is not None:
+        print("existing session, session_id:\t", current_session.get('session_id'))
+    else:
+        current_session['session_id'] = uuid.uuid1()
+        print("new session, session_id:\t", current_session.get('session_id'))
+    return current_session['session_id']
+
+
+def check_user_bind(current_session):
+    """
+    检查用户是否绑定，如果没有绑定则重定向到index
+    :param current_session: 当前session
+    :return: 当前session
+    """
+    if current_session.get('user_id') is None:
+        return False
+    return True
+
+
+def get_user_info(user_id):
+    """
+    获取用户信息
+    :param user_id: 用户id
+    :return: 用户信息
+    """
+    lock.acquire()
+    user_info = all_user_dict.get(user_id)
+    lock.release()
+    return user_info
 
 
 # 进入主页
@@ -88,9 +117,13 @@ def load_messages():
     :return: 聊天记录
     """
     check_session(session)
-    messages_history = session.get('messages_history')
+    if session.get('user_id') is None:
+        messages_history = [{"role": "assistant", "content": "当前会话为首次请求，请输入已有用户id或创建新的用户id"},
+                            {"role": "assistant", "content": "已有用户id请在输入框中输入，创建新的用户id请在输入框中输入new_user_id:xxx"}]
+    else:
+        user_info = get_user_info(session.get('user_id'))
+        messages_history = user_info['messages_history']
     return messages_history
-
 
 
 @app.route('/returnMessage', methods=['GET', 'POST'])
@@ -101,27 +134,36 @@ def return_message():
     """
     check_session(session)
     send_message = request.values.get("send_message")
-    chat_with_history = session.get('chat_with_history')
-    messages_history = session.get('messages_history')
     print("用户发送的消息：" + send_message)
-    content = handle_messages_get_response(send_message, messages_history, chat_with_history)
-    session['messages_history'] = messages_history
-    # 解析换行符和空格以及tab制表符到html
-    # content = content.replace("\n", "<br>")
-    # content = content.replace(" ", "&nbsp;")
-    # # content = content.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;")
-    #
-    # # 按顺序遍历将代码开始标识符```转换为<pre>，结束标识符```转换为</pre>，两个pre代码框内灰白色
-    # content_list = content.split("```")
-    # for i in range(len(content_list)):
-    #     if i % 2 == 1:
-    #         content_list[i] = "<pre>" + content_list[i] + "</pre>"
-    # content = "".join(content_list)
-    data = {
-        "content": content,
-        "content_id": f"content_id{len(messages_history) - 1}"
-    }
-    return data
+    if session.get('user_id') is None:
+        if send_message.strip().startswith("new_user_id:"):
+            user_id = send_message.split(":")[1]
+            session['user_id'] = user_id
+            lock.acquire()
+            all_user_dict.put(user_id, {"chat_with_history": False, "messages_history": [{"role": "assistant", "content": f"当前对话的用户id为 `{user_id}`"}]})        # 默认普通对话
+            lock.release()
+            print("创建新的用户id:\t", user_id)
+            return {"content": "创建新的用户id成功，可以开始对话了"}
+        else:
+            user_id = send_message.strip()
+            user_info = get_user_info(user_id)
+            if user_info is None:
+                return {"content": "用户id不存在，请重新输入或创建新的用户id"}
+            else:
+                session['user_id'] = user_id
+                print("已有用户id:\t", user_id)
+                # 重定向到index
+                return redirect(url_for('index'))
+    else:
+        user_info = get_user_info(session.get('user_id'))
+        messages_history = user_info['messages_history']
+        chat_with_history = user_info['chat_with_history']
+        content = handle_messages_get_response(send_message, messages_history, chat_with_history)
+        data = {
+            "content": content,
+            "content_id": f"content_id{len(messages_history) - 1}"
+        }
+        return data
 
 
 @app.route('/changeModeNormal', methods=['GET'])
@@ -130,7 +172,11 @@ def change_mode_normal():
     开启普通对话模式
     :return:
     """
-    session['chat_with_history'] = False
+    check_session(session)
+    if not check_user_bind(session):
+        return "-1"
+    user_info = get_user_info(session.get('user_id'))
+    user_info['chat_with_history'] = False
     print("开启普通对话")
     return "0"
 
@@ -141,30 +187,33 @@ def change_mode_continuous():
     开启连续对话模式
     :return:
     """
-    session['chat_with_history'] = True
+    check_session(session)
+    if not check_user_bind(session):
+        return "-1"
+    user_info = get_user_info(session.get('user_id'))
+    user_info['chat_with_history'] = True
     print("开启连续对话")
     return "1"
 
 
-@app.route('/resetHistory', methods=['GET'])
+@app.route('/deleteHistory', methods=['GET'])
 def reset_history():
     """
     清空上下文
     :return:
     """
-    session['messages_history'] = []
-    print("清空上下文")
+    check_session(session)
+    if not check_user_bind(session):
+        return "-1"
+    user_info = get_user_info(session.get('user_id'))
+    user_info['messages_history'] = [user_info['messages_history'][0]]
+    print("清空历史记录")
     return "2"
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        if len(openai.api_key) == 0:
-            # 退出程序
-            print("请在openai官网注册账号，获取api_key填写至程序内或命令行参数中")
-            exit()
-    else:
-        openai.api_key = sys.argv[1]
-        print("使用命令行参数作为api_key")
-
+    if len(openai.api_key) == 0:
+        # 退出程序
+        print("请在openai官网注册账号，获取api_key填写至程序内或命令行参数中")
+        exit()
     app.run(host="0.0.0.0", port=5000, debug=True)
