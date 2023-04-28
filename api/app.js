91ced74b8522ae5732e9fe3ced99487bab633f2a
yangliang_surface <mail.liangyang@gmail.com>
2023-04-29 00:30:47 +0800

    测试2

--- api/app.js
+++ api/app.js
@@ -160,5 +160,8 @@ app.get('/selectChat', (req, res) => {
    res.send({"code": 201})
 });
 
+app.listen( process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
+});
 
 module.exports = app;
